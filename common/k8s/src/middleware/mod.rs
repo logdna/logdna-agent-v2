b9c84639c2900924b65fcca5d0ed346f22872ef9
CJP10 <14205938+CJP10@users.noreply.github.com>
2020-05-07 21:51:59 -0400

    Split k8s middleware into two pieces (#3)
    * bump 2.1.9-beta2
    
    * split k8s middlewares into two pieces
    
    * fix beta version

---
+++ common/k8s/src/middleware/mod.rs
@@ -0,0 +1,22 @@
+use regex::Regex;
+
+mod deduplication;
+mod metadata;
+
+pub use deduplication::*;
+pub use metadata::*;
+
+lazy_static! {
+    static ref K8S_REG: Regex = Regex::new(
+        r#"^/var/log/containers/([a-z0-9A-Z\-.]+)_([a-z0-9A-Z\-.]+)_([a-z0-9A-Z\-.]+)-([a-z0-9]{64}).log$"#
+    ).unwrap_or_else(|e| panic!("K8S_REG Regex::new() failed: {}", e));
+}
+
+
+fn parse_container_path(path: &str) -> Option<(String, String)> {
+    let captures = K8S_REG.captures(path)?;
+    Some((
+        captures.get(1)?.as_str().into(),
+        captures.get(2)?.as_str().into(),
+    ))
+}
