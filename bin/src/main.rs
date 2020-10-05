5ec9e609ad3243df9bc0c11af5f3f2ffd982238b
Chris Nixon <chris.nixon@sigma.me.uk>
2020-10-05 17:41:26 +0100

    Refactor fs tailer to return a Stream
    Refactor the fs tailer to return a Stream<Item=Vec<LineBuilder>> and
    update the agent's main to consume it.
    
    Internal Ref: LOG-5685

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -5,23 +5,22 @@ use std::path::PathBuf;
 use std::thread::spawn;
 
 use config::Config;
-use fs::source::FSSource;
+use fs::tail::Tailer as FSSource;
+use futures::StreamExt;
 use http::client::Client;
 #[cfg(use_systemd)]
 use journald::source::JournaldSource;
 use k8s::middleware::K8sMetadata;
 use metrics::Metrics;
 use middleware::Executor;
-use source::SourceReader;
+use pin_utils::pin_mut;
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::thread::sleep;
-use std::time::Duration;
+
+use tokio::runtime::Runtime;
 
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-static SLEEP_DURATION: Duration = Duration::from_millis(10);
 
 // Statically include the CARGO_PKG_NAME and CARGO_PKG_VERSIONs in the binary
 // and export under the PKG_NAME and PKG_VERSION symbols.
@@ -32,13 +31,13 @@ pub static PKG_NAME: &str = env!("CARGO_PKG_NAME");
 #[no_mangle]
 pub static PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 
-#[cfg(use_systemd)]
-fn register_journald_source(source_reader: &mut SourceReader) {
-    source_reader.register(JournaldSource::new());
-}
+// #[cfg(use_systemd)]
+// fn register_journald_source(source_reader: &mut SourceReader) {
+//     source_reader.register(JournaldSource::new());
+// }
 
-#[cfg(not(use_systemd))]
-fn register_journald_source(_source_reader: &mut SourceReader) {}
+// #[cfg(not(use_systemd))]
+// fn register_journald_source(_source_reader: &mut SourceReader) {}
 
 fn main() {
     env_logger::init();
@@ -67,22 +66,32 @@ fn main() {
             Err(e) => warn!("{}", e),
         };
     }
-
-    let mut source_reader = SourceReader::new();
-    register_journald_source(&mut source_reader);
-    source_reader.register(FSSource::new(config.log.dirs, config.log.rules));
-
     executor.init();
 
-    loop {
-        source_reader.drain(Box::new(|lines| {
-            if let Some(lines) = executor.process(lines) {
-                for line in lines {
-                    client.borrow_mut().send(line)
+    let mut fs_tailer_buf = [0u8; 4096];
+    let mut fs_source = FSSource::new(config.log.dirs, config.log.rules);
+    // Create the runtime
+    let mut rt = Runtime::new().unwrap();
+
+    // Execute the future, blocking the current thread until completion
+    rt.block_on(async {
+        let fs_source = fs_source
+            .process(&mut fs_tailer_buf)
+            .expect("except Failed to create FS Tailer");
+        pin_mut!(fs_source);
+
+        let mut sources = futures::stream::SelectAll::new();
+        sources.push(&mut fs_source);
+
+        sources
+            .for_each(|lines| async {
+                if let Some(lines) = executor.process(lines) {
+                    for line in lines {
+                        // TODO upgrade to async hyper
+                        client.borrow_mut().send(line)
+                    }
                 }
-            }
-        }));
-        client.borrow_mut().poll();
-        sleep(SLEEP_DURATION);
-    }
+            })
+            .await
+    });
 }
--- common/fs/src/cache/watch.rs
+++ common/fs/src/cache/watch.rs
@@ -2,6 +2,15 @@ use inotify::{EventMask, Inotify, WatchDescriptor, WatchMask};
 use std::ffi::OsString;
 use std::io;
 use std::path::Path;
+use std::sync::Arc;
+
+use futures::future::Either;
+use futures::{Stream, StreamExt};
+use tokio::time::Instant;
+
+use tokio::sync::Mutex;
+
+const INOTIFY_EVENT_GRACE_PERIOD_MS: u64 = 10;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum WatchEvent {
@@ -35,6 +44,15 @@ pub enum WatchEvent {
     Overflow,
 }
 
+#[derive(std::fmt::Debug)]
+enum EventOrInterval<T>
+where
+    T: std::fmt::Debug,
+{
+    Interval(Instant),
+    Event(Result<inotify::Event<T>, std::io::Error>),
+}
+
 pub struct Watcher {
     inotify: Inotify,
 }
@@ -55,82 +73,211 @@ impl Watcher {
         self.inotify.rm_watch(wd)
     }
 
-    pub fn read_events(&mut self, buffer: &mut [u8]) -> io::Result<Vec<WatchEvent>> {
-        let mut events = Vec::new();
-        for raw_event in self.inotify.read_events(buffer)? {
-            if raw_event.mask.contains(EventMask::MOVED_FROM) {
-                let mut found_match = false;
-                for event in events.iter_mut() {
-                    match event {
-                        WatchEvent::MovedTo { wd, name, cookie } => {
-                            if *cookie == raw_event.cookie {
-                                *event = WatchEvent::Move {
-                                    from_wd: raw_event.wd.clone(),
-                                    from_name: raw_event.name.unwrap().to_os_string(),
-                                    to_wd: wd.clone(),
-                                    to_name: name.clone(),
-                                };
-                                found_match = true;
-                                break;
-                            }
-                        }
-                        _ => continue,
-                    };
-                }
+    pub fn event_stream<'a>(
+        &mut self,
+        buffer: &'a mut [u8],
+    ) -> std::io::Result<WatchEventStream<'a>> {
+        Ok(WatchEventStream {
+            event_stream: self.inotify.event_stream(buffer)?,
+        })
+    }
+}
 
-                if !found_match {
-                    events.push(WatchEvent::MovedFrom {
-                        wd: raw_event.wd.clone(),
-                        name: raw_event.name.unwrap().to_os_string(),
-                        cookie: raw_event.cookie,
-                    });
-                }
-            } else if raw_event.mask.contains(EventMask::MOVED_TO) {
-                let mut found_match = false;
-                for event in events.iter_mut() {
-                    match event {
-                        WatchEvent::MovedFrom { wd, name, cookie } => {
-                            if *cookie == raw_event.cookie {
-                                *event = WatchEvent::Move {
-                                    from_wd: wd.clone(),
-                                    from_name: name.clone(),
-                                    to_wd: raw_event.wd.clone(),
-                                    to_name: raw_event.name.unwrap().to_os_string(),
-                                };
-                                found_match = true;
-                                break;
-                            }
-                        }
-                        _ => continue,
-                    };
-                }
+pub struct WatchEventStream<'a> {
+    event_stream: inotify::EventStream<&'a mut [u8]>,
+}
 
-                if !found_match {
-                    events.push(WatchEvent::MovedTo {
-                        wd: raw_event.wd.clone(),
-                        name: raw_event.name.unwrap().to_os_string(),
-                        cookie: raw_event.cookie,
-                    });
+impl<'a> WatchEventStream<'a> {
+    pub fn into_stream(self) -> impl Stream<Item = Result<WatchEvent, std::io::Error>> + 'a {
+        let unmatched_move_to: Arc<Mutex<Vec<(Instant, WatchEvent)>>> =
+            Arc::new(Mutex::new(Vec::new()));
+        let unmatched_move_from: Arc<Mutex<Vec<(Instant, WatchEvent)>>> =
+            Arc::new(Mutex::new(Vec::new()));
+        // Interleave inotify events with a heartbeat every 1 second
+        // heartbeat is used to ensure unpaired MOVED_TO and MOVED_FROM
+        // correctly generate events.
+
+        let events = futures::stream::select(
+            self.event_stream.map(EventOrInterval::Event),
+            tokio::time::interval(tokio::time::Duration::from_millis(
+                INOTIFY_EVENT_GRACE_PERIOD_MS,
+            ))
+            .map(EventOrInterval::Interval),
+        );
+
+        events
+            .map(move |raw_event_or_interval| {
+                {
+                    match raw_event_or_interval {
+                        EventOrInterval::Event(raw_event) => Either::Left(futures::stream::once({
+                            let unmatched_move_to = unmatched_move_to.clone();
+                            let unmatched_move_from = unmatched_move_from.clone();
+                            async move {
+                                match raw_event {
+                                    Ok(raw_event) => {
+                                        Ok(if raw_event.mask.contains(EventMask::MOVED_FROM) {
+                                            // Check if we have seen the corresponding MOVED_TO
+                                            let mut unmatched_move_to =
+                                                unmatched_move_to.lock().await;
+                                            if let Some(idx) =
+                                                unmatched_move_to.iter().position(|(_, event)| {
+                                                    if let WatchEvent::MovedTo { cookie, .. } =
+                                                        event
+                                                    {
+                                                        *cookie == raw_event.cookie
+                                                    } else {
+                                                        false
+                                                    }
+                                                })
+                                            {
+                                                // If we have seen the corresponding MOVED_TO remove it
+                                                // from the unmatched vec and return a Move
+                                                if let (_, WatchEvent::MovedTo { wd, name, .. }) =
+                                                    unmatched_move_to.swap_remove(idx)
+                                                {
+                                                    Some(WatchEvent::Move {
+                                                        from_wd: raw_event.wd.clone(),
+                                                        from_name: raw_event.name.unwrap(),
+                                                        to_wd: wd,
+                                                        to_name: name,
+                                                    })
+                                                } else {
+                                                    None
+                                                }
+                                            } else {
+                                                // If we can't find the corresponding event, store this
+                                                // event in the unmatched_move_from vec
+                                                unmatched_move_from.lock().await.push((
+                                                    Instant::now(),
+                                                    WatchEvent::MovedFrom {
+                                                        wd: raw_event.wd.clone(),
+                                                        name: raw_event.name.unwrap(),
+                                                        cookie: raw_event.cookie,
+                                                    },
+                                                ));
+                                                None
+                                            }
+                                        } else if raw_event.mask.contains(EventMask::MOVED_TO) {
+                                            let mut unmatched_move_from =
+                                                unmatched_move_from.lock().await;
+                                            if let Some(idx) =
+                                                unmatched_move_from.iter().position(|(_, event)| {
+                                                    if let WatchEvent::MovedFrom {
+                                                        cookie, ..
+                                                    } = event
+                                                    {
+                                                        *cookie == raw_event.cookie
+                                                    } else {
+                                                        false
+                                                    }
+                                                })
+                                            {
+                                                // If we have seen the corresponding MOVED_FROM remove it
+                                                // from the unmatched vec and return a Move
+                                                if let (_, WatchEvent::MovedFrom { wd, name, .. }) =
+                                                    unmatched_move_from.swap_remove(idx)
+                                                {
+                                                    Some(WatchEvent::Move {
+                                                        from_wd: wd,
+                                                        from_name: name,
+                                                        to_wd: raw_event.wd.clone(),
+                                                        to_name: raw_event.name.unwrap(),
+                                                    })
+                                                } else {
+                                                    None
+                                                }
+                                            } else {
+                                                // If we can't find the corresponding event, store this
+                                                // event in the unmatched_move_to vec
+                                                unmatched_move_to.lock().await.push((
+                                                    Instant::now(),
+                                                    WatchEvent::MovedTo {
+                                                        wd: raw_event.wd.clone(),
+                                                        name: raw_event.name.unwrap(),
+                                                        cookie: raw_event.cookie,
+                                                    },
+                                                ));
+                                                None
+                                            }
+                                        } else if raw_event.mask.contains(EventMask::CREATE) {
+                                            Some(WatchEvent::Create {
+                                                wd: raw_event.wd.clone(),
+                                                name: raw_event.name.unwrap(),
+                                            })
+                                        } else if raw_event.mask.contains(EventMask::DELETE) {
+                                            Some(WatchEvent::Delete {
+                                                wd: raw_event.wd.clone(),
+                                                name: raw_event.name.unwrap(),
+                                            })
+                                        } else if raw_event.mask.contains(EventMask::MODIFY) {
+                                            Some(WatchEvent::Modify {
+                                                wd: raw_event.wd.clone(),
+                                            })
+                                        } else if raw_event.mask.contains(EventMask::Q_OVERFLOW) {
+                                            Some(WatchEvent::Overflow)
+                                        } else {
+                                            None
+                                        })
+                                    }
+                                    Err(e) => Err(e),
+                                }
+                            }
+                        })),
+                        EventOrInterval::Interval(now) => {
+                            Either::Right({
+                                let unmatched_move_to = unmatched_move_to.clone();
+                                let unmatched_move_from = unmatched_move_from.clone();
+                                {
+                                    // TODO: hoist this buffer out into an argument
+                                    let mut events = vec![];
+                                    {
+                                        let mut unmatched_move_to = unmatched_move_to
+                                            .try_lock()
+                                            .expect("Couldn't lock unmatched_move_to");
+                                        while let Some(idx) =
+                                            unmatched_move_to.iter().position(|(instant, _)| {
+                                                now - tokio::time::Duration::from_millis(
+                                                    INOTIFY_EVENT_GRACE_PERIOD_MS,
+                                                ) > *instant
+                                            })
+                                        {
+                                            events.push(Ok(Some(
+                                                unmatched_move_to.swap_remove(idx).1,
+                                            )));
+                                        }
+                                    }
+                                    {
+                                        let mut unmatched_move_from = unmatched_move_from
+                                            .try_lock()
+                                            .expect("Couldn't lock unmatched_move_to");
+                                        while let Some(idx) =
+                                            unmatched_move_from.iter().position(|(instant, _)| {
+                                                now - tokio::time::Duration::from_millis(
+                                                    INOTIFY_EVENT_GRACE_PERIOD_MS,
+                                                ) > *instant
+                                            })
+                                        {
+                                            events.push(Ok(Some(
+                                                unmatched_move_from.swap_remove(idx).1,
+                                            )));
+                                        }
+                                    }
+                                    // unmatched_move_to.position
+                                    futures::stream::iter(events)
+                                }
+                            })
+                        }
+                    }
                 }
-            } else if raw_event.mask.contains(EventMask::CREATE) {
-                events.push(WatchEvent::Create {
-                    wd: raw_event.wd.clone(),
-                    name: raw_event.name.unwrap().to_os_string(),
-                });
-            } else if raw_event.mask.contains(EventMask::DELETE) {
-                events.push(WatchEvent::Delete {
-                    wd: raw_event.wd.clone(),
-                    name: raw_event.name.unwrap().to_os_string(),
-                });
-            } else if raw_event.mask.contains(EventMask::MODIFY) {
-                events.push(WatchEvent::Modify {
-                    wd: raw_event.wd.clone(),
-                });
-            } else if raw_event.mask.contains(EventMask::Q_OVERFLOW) {
-                events.push(WatchEvent::Overflow);
-            }
-        }
-        Ok(events)
+            })
+            .flatten()
+            // Unwrap the inner Option and discard unmatched events
+            .filter_map(|event: Result<Option<WatchEvent>, std::io::Error>| async {
+                match event {
+                    Ok(None) => None,
+                    event => Some(event.map(|e| e.unwrap())),
+                }
+            })
     }
 }
 
