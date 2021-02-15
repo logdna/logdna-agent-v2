acd475927760befceeaa6854a725a007a344e5af
Jorge Bay-Gondra <jorgebaygondra@gmail.com>
2021-02-15 18:47:50 +0100

    Fix follow_links() loop and lookup() (#93)
    fix: Fix follow_links() loop and lookup()
    chore: fix cargo-audit

--- common/fs/src/tail.rs
+++ common/fs/src/tail.rs
@@ -174,7 +174,7 @@ impl Tailer {
                                 debug!("Delete Event");
                                 if !paths.is_empty() {
                                     if let Entry::Symlink { link, .. } = entry.borrow().deref() {
-                                        if let Ok(Some(real_entry)) = fs.lookup(link, &entries) {
+                                        if let Some(real_entry) = fs.lookup(link, &entries) {
                                             if let Some(r_entry) = entries.get(real_entry) {
                                                 entry = r_entry
                                             }
@@ -204,8 +204,8 @@ impl Tailer {
                             // if Some(..) = entries.get(key) clauses)
                             let mut entries = fs.entries.borrow_mut();
                             if entries.remove(entry_ptr).is_some() {
-                                debug!(
-                                    "Entry was removed from the map, new length: {}",
+                                info!(
+                                    "Removed file information, currently tracking {} files",
                                     entries.len());
                             }
                         }
