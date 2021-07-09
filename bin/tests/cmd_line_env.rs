998f44876dcedab784cfd64975b95606d26199ee
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2021-07-09 13:43:06 +0200

    Test fix

--- bin/tests/cmd_line_env.rs
+++ bin/tests/cmd_line_env.rs
@@ -37,13 +37,13 @@ fn test_command_line_arguments_help() {
         "-k, --key",
         "The ingestion key associated with your LogDNA account",
         "-d, --logdir",
-        "Adds log directories to scan, in addition to the default (/var/log)",
+        "Adds log directories to scan, in addition to the default",
         "-t, --tags",
         "List of tags metadata to attach to lines forwarded from this agent",
     ]
     .iter()
     .for_each(|m| {
-        assert!(contains(*m).eval(stdout));
+        assert!(contains(*m).eval(stdout), "Not found: {}", *m);
     });
 }
 
@@ -177,7 +177,7 @@ fn test_command_line_arguments_should_set_config() {
         },
         |d| {
             assert!(contains("tags: \"a,b\"").eval(d));
-            assert!(is_match(r"log:\s+dirs:\s+\- /var/log/\s+\- /d1/\s+\- /d2/")
+            assert!(is_match(r"log:\s+dirs:\s+\- /var/log\s+\- /d1/\s+\- /d2/")
                 .unwrap()
                 .eval(d));
             assert!(contains("host: remotehost").eval(d));
