c6037c62346a0b6fa990b57dfcbde923c55d6496
Chris Nixon <chris.nixon@sigma.me.uk>
2021-09-09 17:48:59 +0100

    Extract proptest strategies out into test module

--- common/test/types/src/lib.rs
+++ common/test/types/src/lib.rs
@@ -1,14 +1,2 @@
-use proptest::collection::vec;
-use proptest::prelude::Strategy;
-use proptest::string::string_regex;
-
-pub fn random_line_string_vec(
-    min_size: usize,
-    max_size: usize,
-) -> impl Strategy<Value = Vec<String>> {
-    vec(
-        (min_size..max_size)
-            .prop_flat_map(|i| string_regex(&format!("[a-zA-Z0-9]{{1,64}}{}", i)).unwrap()),
-        min_size..max_size,
-    )
-}
+#[cfg(test)]
+pub mod strategies;
