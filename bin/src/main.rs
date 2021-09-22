099149f80850287f60bfcc907c6b557a5c56e6a5
Chris Nixon <chris.nixon@sigma.me.uk>
2021-09-22 10:20:43 +0100

    Log 10787 cleanup retry files (#224)
    * Extract proptest strategies out into test module
    * Factor out clienterror handling
    * Test retry file cleanup

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -9,8 +9,8 @@ use env_logger::Env;
 use fs::tail::Tailer as FSSource;
 use futures::StreamExt;
 use http::batch::TimedRequestBatcherStreamExt;
-use http::client::Client;
-use http::retry::retry;
+use http::client::{Client, ClientError, SendStatus};
+use http::retry::{retry, RetryItem};
 
 #[cfg(feature = "libjournald")]
 use journald::libjournald::source::create_source;
@@ -295,13 +295,49 @@ async fn main() {
         .map(|b| async { b })
         .buffered(1);
 
+    fn handle_client_error<T>(e: ClientError<T>)
+    where
+        T: Send + 'static,
+    {
+        match e {
+            ClientError::BadRequest(s) => {
+                warn!("bad http request: {}", s);
+            }
+            ClientError::Http(e) => {
+                warn!("failed sending http request: {}", e);
+            }
+            ClientError::Retry(r) => {
+                error!("failed to retry request: {}", r);
+            }
+            ClientError::State(s) => {
+                error!("Unable to flush state to disk. error: {}", s);
+            }
+        }
+    }
+
+    fn handle_send_status(s: SendStatus) {
+        match s {
+            SendStatus::Retry(e) => {
+                warn!("failed sending http request, retrying: {}", e);
+            }
+            SendStatus::RetryTimeout => {
+                warn!("failed sending http request, retrying: request timed out!");
+            }
+            _ => {}
+        }
+    }
+
     let lines_driver = body_offsets_stream.for_each(|body_offsets| async {
         match body_offsets {
             Ok((body, offsets)) => {
-                client
+                match client
                     .borrow()
                     .send(body, Some(offsets.items_as_ref()))
                     .await
+                {
+                    Ok(s) => handle_send_status(s),
+                    Err(e) => handle_client_error(e),
+                }
             }
             Err(e) => error!("Couldn't batch lines {:?}", e),
         }
@@ -309,11 +345,28 @@ async fn main() {
 
     let retry_driver = retry_stream.into_stream().for_each(|body_offsets| async {
         match body_offsets {
-            Ok((body, offsets)) => {
-                client
+            Ok(item) => {
+                let RetryItem {
+                    body_buffer,
+                    offsets,
+                    path,
+                } = item;
+                match client
                     .borrow()
-                    .send(body, offsets.as_ref().map(|o| o.as_ref()))
+                    .send(body_buffer, offsets.as_ref().map(|o| o.as_ref()))
                     .await
+                {
+                    Ok(s) => match s {
+                        SendStatus::Sent => {
+                            debug!("cleaned up retry file");
+                            if let Err(e) = std::fs::remove_file(path) {
+                                error!("couldn't clean up retry file {}", e)
+                            }
+                        }
+                        _ => handle_send_status(s),
+                    },
+                    Err(e) => handle_client_error(e),
+                }
             }
             Err(e) => error!("Couldn't batch lines {:?}", e),
         }
--- common/http/src/retry.rs
+++ common/http/src/retry.rs
@@ -55,6 +55,22 @@ struct DiskRead {
     body: IngestBody,
 }
 
+pub struct RetryItem {
+    pub body_buffer: IngestBodyBuffer,
+    pub offsets: Option<Vec<Offset>>,
+    pub path: PathBuf,
+}
+
+impl RetryItem {
+    fn new(body_buffer: IngestBodyBuffer, offsets: Option<Vec<Offset>>, path: PathBuf) -> Self {
+        Self {
+            body_buffer,
+            offsets,
+            path,
+        }
+    }
+}
+
 impl Retry {
     pub fn new(
         directory: PathBuf,
@@ -113,9 +129,7 @@ impl Retry {
         Ok((offsets, body))
     }
 
-    pub fn into_stream(
-        self,
-    ) -> impl Stream<Item = Result<(IngestBodyBuffer, Option<Vec<Offset>>), Error>> {
+    pub fn into_stream(self) -> impl Stream<Item = Result<RetryItem, Error>> {
         stream::unfold(self, |state| async move {
             loop {
                 // Try to populate retry queue
@@ -140,7 +154,10 @@ impl Retry {
                         Ok((offsets, ingest_body)) => {
                             match IntoIngestBodyBuffer::into(ingest_body).await {
                                 Ok(body_buffer) => {
-                                    return Some((Ok((body_buffer, offsets)), state))
+                                    return Some((
+                                        Ok(RetryItem::new(body_buffer, offsets, path)),
+                                        state,
+                                    ))
                                 }
                                 Err(e) => return Some((Err(e.into()), state)),
                             }
@@ -263,7 +280,7 @@ mod tests {
         #[test]
         fn roundtrip(
 
-            inp in (0..10usize)
+            inp in (0..1024usize)
                 .prop_flat_map(|size|(Just(size),
                                       proptest::collection::vec(line_st(offset_st(1024)), size)
                 ))) {
@@ -272,23 +289,40 @@ mod tests {
             let dir_path = format!("{}/", dir.path().to_str().unwrap());
 
             let (size, lines) = inp;
-            let (retrier, retry_stream) = retry(dir_path.into(), Duration::from_millis(1000), Duration::from_millis(0));
+            let (retrier, retry_stream) = retry(dir_path.clone().into(), Duration::from_millis(1000), Duration::from_millis(0));
             let (results, retry_results) =
-                tokio_test::block_on(async {
+                tokio_test::block_on({
+                    let dir_path = dir_path.clone();
+
                     let batch_stream = stream::iter(lines.iter()).timed_request_batches(5_000, Duration::new(1, 0));
-                    let results = batch_stream.collect::<Vec<_>>().await;
+                    async move {
+                        let results = batch_stream.collect::<Vec<_>>().await;
 
-                    // Retry all the results and assert they come off the stream
-                    for body_offsets in results.iter() {
-                        let (body, offsets) = body_offsets.as_ref().unwrap();
-                        retrier.retry(Some(offsets.items_as_ref()), body).await.unwrap()
-                    }
+                        // Check there are no retry files
+                        assert_eq!(std::fs::read_dir(&dir_path).unwrap().count(), 0);
+                        // Retry all the results and assert they come off the stream
+                        for (idx, body_offsets) in results.iter().enumerate() {
+                            let (body, offsets) = body_offsets.as_ref().unwrap();
+                            retrier.retry(Some(offsets.items_as_ref()), body).await.unwrap();
+                            // Check there are the right number of retry files
+                            assert_eq!(std::fs::read_dir(&dir_path).unwrap().count(), idx + 1);
+                        }
 
-                    let retry_results = retry_stream.into_stream()
-                        .take(results.len())
-                        .collect::<Vec<_>>().await;
-                    (results, retry_results)
-                });
+                        let results_len = results.len();
+                        let retry_results = retry_stream.into_stream()
+                            .take(results_len)
+                            .enumerate()
+                            .map({
+                                let dir_path = dir_path.clone();
+                                move |(idx, res)| {
+                                    assert_eq!(std::fs::read_dir(&dir_path).unwrap().count(), results_len - (idx + 1));
+                                    res
+                                }})
+                            .collect::<Vec<_>>().await;
+                        (results, retry_results)
+                }});
+
+            assert_eq!(std::fs::read_dir(&dir_path).unwrap().count(), 0);
 
             assert_eq!(results.len(), retry_results.len());
 
@@ -320,8 +354,8 @@ mod tests {
             // Grab retries and check we got them all
             let retry_results = retry_results.into_iter().map(move |body_offsets|{
                 let mut buf = String::new();
-                let (body, _offsets) = body_offsets.unwrap();
-                body.reader()
+                let body = body_offsets.unwrap();
+                body.body_buffer.reader()
                 .read_to_string(&mut buf)
                 .unwrap();
                 let mut body: HashMap<String, Vec<Line>> = serde_json::from_str(&buf).unwrap();
