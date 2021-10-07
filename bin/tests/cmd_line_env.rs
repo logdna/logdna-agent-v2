8335efb82f7c2a3b7b5779045a8a5f5ed673d182
Dan Hable <dan@danhable.com>
2021-10-07 10:42:45 -0500

    Supporting IBM specific host env var

--- bin/tests/cmd_line_env.rs
+++ bin/tests/cmd_line_env.rs
@@ -175,6 +175,22 @@ journald: {{}}
 
 #[test]
 #[cfg_attr(not(feature = "integration_tests"), ignore)]
+fn test_ibm_legacy_host_env_var() {
+    let mut cmd = get_bin_command();
+    let output: Output = cmd
+        .env_clear()
+        .env("LOGDNA_LOGHOST", "other.api.logdna.test")
+        .arg("-l")
+        .unwrap();
+
+    assert!(output.status.success());
+
+    let stdout = from_utf8(&output.stdout).unwrap();
+    assert!(contains("host: other.api.logdna.test").eval(stdout));
+}
+
+#[test]
+#[cfg_attr(not(feature = "integration_tests"), ignore)]
 fn test_list_config_from_env() -> io::Result<()> {
     let mut cmd = get_bin_command();
     let output: Output = cmd
