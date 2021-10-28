9b6d02939b30633b31768593c33b742b1a3e660a
Dan Hable <dan@danhable.com>
2021-10-28 11:45:27 -0500

    refactor: Follow streams package code layout

--- common/fs/src/stream_utils.rs
+++ common/fs/src/stream_utils.rs
@@ -8,22 +8,23 @@ pub struct Recover<St: Stream + Unpin, C, P> {
     producer: Arc<Mutex<P>>,
 }
 
-impl<St: Stream + Unpin, C: Fn(&St::Item) -> bool, P: FnMut() -> St> Recover<St, C, P> {
-    /// Constructs a new stream that obtains elements from an underlying stream, created from
-    /// the `produce` function and will create new streams using `produce` when the `recover_check`
-    /// returns true.
-    pub fn recover(recover_check: C, producer: P) -> Recover<St, C, P> {
-        let producer = Arc::new(Mutex::new(producer));
-        let inner = {
-            let mut produce_fn = producer.lock().unwrap();
-            Arc::new(Mutex::new(produce_fn()))
-        };
+/// Constructs a new stream that obtains elements from an underlying stream, created from
+/// the `produce` function and will create new streams using `produce` when the `recover_check`
+/// returns true.
+pub fn recover<St: Stream + Unpin, C: Fn(&St::Item) -> bool, P: FnMut() -> St>(
+    recover_check: C,
+    producer: P,
+) -> Recover<St, C, P> {
+    let producer = Arc::new(Mutex::new(producer));
+    let inner = {
+        let mut produce_fn = producer.lock().unwrap();
+        Arc::new(Mutex::new(produce_fn()))
+    };
 
-        Recover {
-            inner,
-            recover_check,
-            producer,
-        }
+    Recover {
+        inner,
+        recover_check,
+        producer,
     }
 }
 
@@ -72,7 +73,7 @@ mod test {
         let should_recover = |_: &String| false;
         let data_producer = || futures::stream::empty();
 
-        let test_st = Recover::recover(should_recover, data_producer).collect::<Vec<String>>();
+        let test_st = recover(should_recover, data_producer).collect::<Vec<String>>();
         let result = tokio_test::block_on(test_st);
 
         assert!(result.is_empty());
@@ -83,7 +84,7 @@ mod test {
         let should_recover = |_: &usize| false;
         let data_producer = || futures::stream::iter(vec![1, 2, 3]);
 
-        let test_st = Recover::recover(should_recover, data_producer).collect::<Vec<usize>>();
+        let test_st = recover(should_recover, data_producer).collect::<Vec<usize>>();
         let result = tokio_test::block_on(test_st);
 
         assert_eq!(result, vec![1, 2, 3]);
@@ -110,7 +111,7 @@ mod test {
 
         // Limit the recovery stream to producing 9 elements. The value at 4 and 8 will be skipped
         // which verifies that the stream was able to correctly recover more than once.
-        let test_st = Recover::recover(should_recover, data_producer)
+        let test_st = recover(should_recover, data_producer)
             .take(8)
             .collect::<Vec<usize>>();
         let result = tokio_test::block_on(test_st);
@@ -141,7 +142,7 @@ mod test {
         // attempts in succession.
         let should_recover = |n: &usize| *n == 2 || *n == 3;
 
-        let test_st = Recover::recover(should_recover, data_producer).collect::<Vec<usize>>();
+        let test_st = recover(should_recover, data_producer).collect::<Vec<usize>>();
         let result = tokio_test::block_on(test_st);
 
         assert_eq!(result, vec![1, 4, 5]);
