2a233e58cfa4c88baab146cbdf91b10815fbb99e
stsantilena <12675156+stsantilena@users.noreply.github.com>
2022-03-29 11:04:27 -0700

    Added lease functions

---
+++ common/k8s/src/lease.rs
@@ -0,0 +1,95 @@
+use std::fmt::Debug;
+use chrono::Utc;
+use k8s_openapi::api::coordination::v1::Lease;
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::MicroTime;
+use kube::api::{Patch, PatchParams, ListParams};
+use kube::core::ObjectList;
+use kube::Api;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LeasePatchSpec {
+    spec: LeasePatchValue
+}
+
+// Non snake case for JSON parsing reasons
+#[derive(Debug, Serialize, Deserialize)]
+struct LeasePatchValue {
+    holderIdentity: Option<String>,
+    acquireTime: MicroTime,
+}
+
+pub async fn get_available_lease(lease_label: &str, lease_client: &Api<Lease>) -> Option<String> {
+    let lease_info = get_lease_list(lease_label, lease_client).await;
+    for lease in lease_info.unwrap().into_iter() {
+        let lease_name = lease.metadata.name.unwrap();
+        match lease.spec.unwrap().holder_identity {
+            Some(lease_owner) => {
+                println!("Lease {} is OWNED by {:?}.", lease_name, lease_owner)
+            }
+            None => {
+                println!("Lease {} NOT OWNED...", lease_name);
+                return Some(lease_name);
+            }
+        }
+    }
+   None
+}
+
+pub async fn claim_lease(lease_name: String, lease_client: &Api<Lease>) {
+    let patch_value = LeasePatchValue{
+        holderIdentity: Some(String::from("agent-2")), // TODO: need to find where this comes from?
+        acquireTime: MicroTime(Utc::now())
+    };
+
+    let patch_spec = LeasePatchSpec {spec: patch_value};
+    let patch = Patch::Merge(patch_spec);
+    let pp = PatchParams::apply(&lease_name);
+    let patch_lease = lease_client.patch(&lease_name, &pp, &patch).await;
+    println!("Lease {} now owned by {:?}",lease_name, patch_lease.unwrap().spec.unwrap().holder_identity);
+}
+
+pub async fn release_lease(lease_name: String, lease_client: &Api<Lease>) {
+    let patch_value = LeasePatchValue{
+        holderIdentity: None,
+        acquireTime: MicroTime(Utc::now())
+    };
+    let patch_spec = LeasePatchSpec {spec: patch_value};
+    let patch = Patch::Merge(patch_spec);
+    let pp = PatchParams::apply(&lease_name);
+    let patch_lease = lease_client.patch(&lease_name, &pp, &patch).await;
+    println!("Lease {} has been released to {:?}", &lease_name, patch_lease.unwrap().spec.unwrap().holder_identity);
+}
+
+async fn get_lease_list(lease_label: &str, lease_client: &Api<Lease>
+) -> Result<ObjectList<Lease>, kube::Error> {
+    let lp = ListParams::default().labels(lease_label);
+    let lease_info = lease_client.list(&lp).await;
+    lease_info
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::{Utc, TimeZone};
+
+    #[tokio::test]
+    async fn test_leasepatchspec_object_serialize() {
+        let test_agent = String::from("test-agent");
+        let test_expected_agent = String::from("test-agent");
+        let test_date = Utc.ymd(2020, 3, 28).and_hms(15, 30, 5);
+        
+        let test_leasepatchvalue = LeasePatchValue {
+            holderIdentity: Some(test_agent),
+            acquireTime: MicroTime(test_date)
+        };
+        
+        let test_leasepatchspec = LeasePatchSpec {
+            spec: test_leasepatchvalue
+        };
+
+        assert_eq!(test_leasepatchspec.spec.holderIdentity.unwrap(), test_expected_agent);
+        assert_eq!(test_leasepatchspec.spec.acquireTime, MicroTime(test_date));
+    }
+}
