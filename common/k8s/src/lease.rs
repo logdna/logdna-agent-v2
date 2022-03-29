6b6ee325910c3009fcc0530774afee87866932bd
stsantilena <12675156+stsantilena@users.noreply.github.com>
2022-03-29 11:51:21 -0700

    Fix lint issues

--- common/k8s/src/lease.rs
+++ common/k8s/src/lease.rs
@@ -1,18 +1,18 @@
-use std::fmt::Debug;
 use chrono::Utc;
 use k8s_openapi::api::coordination::v1::Lease;
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::MicroTime;
-use kube::api::{Patch, PatchParams, ListParams};
+use kube::api::{ListParams, Patch, PatchParams};
 use kube::core::ObjectList;
 use kube::Api;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct LeasePatchSpec {
-    spec: LeasePatchValue
+    spec: LeasePatchValue,
 }
 
 // Non snake case for JSON parsing reasons
+#[allow(non_snake_case)]
 #[derive(Debug, Serialize, Deserialize)]
 struct LeasePatchValue {
     holderIdentity: Option<String>,
@@ -33,63 +33,75 @@ pub async fn get_available_lease(lease_label: &str, lease_client: &Api<Lease>) -
             }
         }
     }
-   None
+    None
 }
 
 pub async fn claim_lease(lease_name: String, lease_client: &Api<Lease>) {
-    let patch_value = LeasePatchValue{
+    let patch_value = LeasePatchValue {
         holderIdentity: Some(String::from("agent-2")), // TODO: need to find where this comes from?
-        acquireTime: MicroTime(Utc::now())
+        acquireTime: MicroTime(Utc::now()),
     };
 
-    let patch_spec = LeasePatchSpec {spec: patch_value};
+    let patch_spec = LeasePatchSpec { spec: patch_value };
     let patch = Patch::Merge(patch_spec);
     let pp = PatchParams::apply(&lease_name);
     let patch_lease = lease_client.patch(&lease_name, &pp, &patch).await;
-    println!("Lease {} now owned by {:?}",lease_name, patch_lease.unwrap().spec.unwrap().holder_identity);
+    println!(
+        "Lease {} now owned by {:?}",
+        &lease_name,
+        patch_lease.unwrap().spec.unwrap().holder_identity
+    );
 }
 
 pub async fn release_lease(lease_name: String, lease_client: &Api<Lease>) {
-    let patch_value = LeasePatchValue{
+    let patch_value = LeasePatchValue {
         holderIdentity: None,
-        acquireTime: MicroTime(Utc::now())
+        acquireTime: MicroTime(Utc::now()),
     };
-    let patch_spec = LeasePatchSpec {spec: patch_value};
+    let patch_spec = LeasePatchSpec { spec: patch_value };
     let patch = Patch::Merge(patch_spec);
     let pp = PatchParams::apply(&lease_name);
     let patch_lease = lease_client.patch(&lease_name, &pp, &patch).await;
-    println!("Lease {} has been released to {:?}", &lease_name, patch_lease.unwrap().spec.unwrap().holder_identity);
+    println!(
+        "Lease {} has been released to {:?}",
+        &lease_name,
+        patch_lease.unwrap().spec.unwrap().holder_identity
+    );
 }
 
-async fn get_lease_list(lease_label: &str, lease_client: &Api<Lease>
+async fn get_lease_list(
+    lease_label: &str,
+    lease_client: &Api<Lease>,
 ) -> Result<ObjectList<Lease>, kube::Error> {
     let lp = ListParams::default().labels(lease_label);
     let lease_info = lease_client.list(&lp).await;
     lease_info
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use chrono::{Utc, TimeZone};
+    use chrono::{TimeZone, Utc};
 
     #[tokio::test]
     async fn test_leasepatchspec_object_serialize() {
         let test_agent = String::from("test-agent");
         let test_expected_agent = String::from("test-agent");
         let test_date = Utc.ymd(2020, 3, 28).and_hms(15, 30, 5);
-        
+
         let test_leasepatchvalue = LeasePatchValue {
             holderIdentity: Some(test_agent),
-            acquireTime: MicroTime(test_date)
-        };
-        
-        let test_leasepatchspec = LeasePatchSpec {
-            spec: test_leasepatchvalue
+            acquireTime: MicroTime(test_date),
         };
 
-        assert_eq!(test_leasepatchspec.spec.holderIdentity.unwrap(), test_expected_agent);
+        let test_leasepatchspec = LeasePatchSpec {
+            spec: test_leasepatchvalue,
+        };
+
+        assert_eq!(
+            test_leasepatchspec.spec.holderIdentity.unwrap(),
+            test_expected_agent
+        );
         assert_eq!(test_leasepatchspec.spec.acquireTime, MicroTime(test_date));
     }
 }
