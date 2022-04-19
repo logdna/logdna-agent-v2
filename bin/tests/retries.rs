d923347aac4b92c94fd040a1bd18cacac3313569
stsantilena <12675156+stsantilena@users.noreply.github.com>
2022-04-19 14:56:03 -0700

    Test ignore IT

--- bin/tests/retries.rs
+++ bin/tests/retries.rs
@@ -82,8 +82,9 @@ async fn test_retry_disk_limit() {
     agent_handle.kill().unwrap();
 }
 
+// IGNORE FOR TESTING
 #[tokio::test]
-#[cfg_attr(not(feature = "integration_tests"), ignore)]
+#[cfg_attr(feature = "integration_tests", ignore)]
 async fn test_retry_location() {
     let _ = env_logger::Builder::from_default_env().try_init();
     let timeout = 200;
@@ -147,8 +148,9 @@ async fn test_retry_location() {
     agent_handle.kill().unwrap();
 }
 
+// IGNORE FOR TESTING
 #[tokio::test]
-#[cfg_attr(not(feature = "integration_tests"), ignore)]
+#[cfg_attr(feature = "integration_tests", ignore)]
 async fn test_retry_after_timeout() {
     let _ = env_logger::Builder::from_default_env().try_init();
     let timeout = 200;
@@ -305,8 +307,9 @@ async fn gen_log_data(file: &mut File) {
     tokio::time::sleep(tokio::time::Duration::from_millis(250)).await;
 }
 
+// IGNORE FOR TESTING
 #[tokio::test]
-#[cfg_attr(not(feature = "integration_tests"), ignore)]
+#[cfg_attr(feature = "integration_tests", ignore)]
 async fn test_retry_metrics_emitted() {
     let _ = env_logger::Builder::from_default_env().try_init();
     let timeout = 200;
