3da921735179e5ac72f7ac47200559a92461b09f
Dmitri Khokhlov <dmitri.khokhlov@logdna.com>
2022-06-12 22:45:54 -0700

    switched to capctl

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -40,8 +40,6 @@ use tokio::time::Duration;
 mod dep_audit;
 mod stream_adapter;
 
-use capng;
-
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
@@ -54,21 +52,46 @@ pub static PKG_NAME: &str = env!("CARGO_PKG_NAME");
 #[no_mangle]
 pub static PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 
-#[tokio::main(flavor = "multi_thread")]
-async fn main() {
-    // must be done at the very beginning
-    if std::env::var_os("KUBERNETES_SERVICE_HOST").is_some()
-        && std::env::var_os(env_vars::LOGDNA_NO_CAP).is_none()
-        && !std::env::current_exe()
-            .unwrap_or_default()
-            .ends_with("-no-cap")
-    {
-        set_capabilities();
-    }
-
+fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     info!("running version: {}", env!("CARGO_PKG_VERSION"));
 
+    // must be done at the very beginning and before other threads started
+    #[cfg(target_os = "linux")]
+    {
+        // apply capabilities only when:
+        // - k8s
+        // - docker
+        // - exe filename ends with "-no-cap" (symlinked)
+        if (std::env::var_os("KUBERNETES_SERVICE_HOST").is_some()
+            || std::path::Path::new("/.dockerenv").exists())
+            && std::env::var_os(env_vars::LOGDNA_NO_CAP).is_none()
+            && !std::env::current_exe()
+                .unwrap_or_default()
+                .ends_with("-no-cap")
+        {
+            match set_capabilities() {
+                Ok(r) if r => debug!("Using Capabilities to bypass filesystem permissions"),
+                _ => warn!("Failed to adopt capabilities to bypass DAC. The agent will only be able to access files accessible to it's user/group"),
+            }
+        }
+        let status =
+            std::fs::read_to_string("/proc/self/status").expect("Failed to read /proc/self/status");
+        let re = regex::Regex::new(r"(?m)^((Cap|Cpu|Seccomp|Groups|Uid|Gid).+?)$").unwrap();
+        for cap in re.captures_iter(status.as_str()) {
+            info!("{}", &cap[0]);
+        }
+    }
+
+    // Set up tokio runtime and block on agent main loop
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(_main())
+}
+
+async fn _main() {
     // Actually use the data to work around a bug in rustc:
     // https://github.com/rust-lang/rust/issues/47384
     dep_audit::get_auditable_dependency_list()
@@ -583,36 +606,33 @@ async fn get_signal() -> &'static str {
     "CTRL+C"
 }
 
-#[cfg(unix)]
-fn set_capabilities() {
-    capng::get_caps_process().unwrap();
+#[cfg(target_os = "linux")]
+fn set_capabilities() -> Result<bool, capctl::Error> {
+    use capctl::caps::{Cap, CapState};
 
-    /*
-       let mut capability_set = Capabilities::new().unwrap();
-       capability_set.reset_all();
+    // Get the caps for the current pid
+    let mut cap_state = CapState::get_current()?;
+    trace!(
+        "initial caps -\npermitted {:?}\neffective {:?}\ninherited {:?}",
+        cap_state.permitted,
+        cap_state.effective,
+        cap_state.inheritable
+    );
+    // this needs: setcap "cap_dac_read_search+p" /work/logdna-agent
+    cap_state.effective.add(Cap::DAC_READ_SEARCH);
+    cap_state.inheritable.add(Cap::DAC_READ_SEARCH);
+    cap_state.set_current()?;
 
-       let current = Capabilities::from_current_proc().unwrap();
-       panic!("Current capabilities - {}", current);
-
-       let _contents = std::fs::read_to_string("/var/log/kernel.log").unwrap();
-
-       let flags = [Capability::CAP_DAC_READ_SEARCH];
-
-       capability_set.update(&flags, Flag::Permitted, true);
-       capability_set.update(&flags, Flag::Effective, true);
-       capability_set.update(&flags, Flag::Inheritable, true);
-
-       match capability_set.apply() {
-           Ok(_) => {
-               let current = Capabilities::from_current_proc().unwrap();
-               info!("New  capabilities - {}", current);
-           }
-           Err(e) => {
-               panic!("Unable to apply capabilities - {}", e);
-           }
-       }
-    */
+    let cap_state = CapState::get_current()?;
+    trace!(
+        "new capabilities -\npermitted {:?}\neffective {:?}\ninherited {:?}",
+        cap_state.permitted,
+        cap_state.effective,
+        cap_state.inheritable
+    );
+    // Check if we have DAC_READ_SEARCH or DAC_OVERRIDE
+    Ok(cap_state
+        .effective
+        .iter()
+        .any(|cap| [Cap::DAC_READ_SEARCH, Cap::DAC_OVERRIDE].contains(&cap)))
 }
-
-#[cfg(windows)]
-fn init_capabilities() {}
