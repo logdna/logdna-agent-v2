d0b898919c41515ea94fb162db76fa8f62dfea61
Chris Nixon <chris.nixon@sigma.me.uk>
2022-07-27 14:31:46 +0100

    Add winres support to agent to allow embedding metadata in exes

--- bin/build.rs
+++ bin/build.rs
@@ -1,4 +1,26 @@
 fn main() {
     #[cfg(feature = "dep_audit")]
     auditable_build::collect_dependency_list();
+
+    // only run if target os is windows
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winres::WindowsResource::new();
+        if cfg!(unix) {
+            // windres tool
+            if let Ok(path) = std::env::var("XWIN_TOOLKIT_BIN_PATH") {
+                res.set_toolkit_path(&path);
+            }
+        }
+        /*
+        // Set more stuff in the resource file
+        res.set_language(0x0409)
+            .set_manifest_file("manifest.xml");
+
+        */
+
+        if let Err(e) = res.compile() {
+            println!("cargo:error={}", e);
+            std::process::exit(1);
+        }
+    }
 }
