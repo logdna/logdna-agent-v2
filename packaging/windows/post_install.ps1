5dd2e464ddfdc03a30031ff24226c3f61d09130b
Dmitri Khokhlov <dmitri.khokhlov@logdna.com>
2022-08-01 11:53:36 -0700

    - working ps1 params

--- packaging/windows/post_install.ps1
+++ packaging/windows/post_install.ps1
@@ -1,4 +1,17 @@
-$ErrorActionPreference = 'Inquire'
+param (
+[string] $INGESTION_KEY,
+[string] $DATAFOLDER
+)
+$ErrorActionPreference = 'Stop'
 sc.exe failure MezmoAgentService reset= 60 actions= restart/15000/restart/15000/restart/15000
 echo $LASTEXITCODE
+
+echo $INGESTION_KEY
+echo $DATAFOLDER
+
 Write-Host
+
+Start-Sleep 20
+
+
+
