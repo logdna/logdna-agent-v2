f5b0dd663d8ac7338d46418e3f4d2642d9da29e1
Dmitri Khokhlov <dmitri.khokhlov@logdna.com>
2022-08-02 11:28:54 -0700

    - functional KEY ps1 param

--- packaging/windows/post_install.ps1
+++ packaging/windows/post_install.ps1
@@ -1,17 +1,22 @@
 param (
 [string] $INGESTION_KEY,
+[string] $INSTALLFOLDER,
 [string] $DATAFOLDER
 )
 $ErrorActionPreference = 'Stop'
-sc.exe failure MezmoAgentService reset= 60 actions= restart/15000/restart/15000/restart/15000
-echo $LASTEXITCODE
 
-echo $INGESTION_KEY
-echo $DATAFOLDER
+# set service recovery params
+sc.exe failure MezmoAgentService reset= 60 actions= restart/15000/restart/15000/restart/15000
+
+if (-not(Test-Path -Path "$DATAFOLDER\logdna.conf" -PathType Leaf)) {
+  # read config template
+  # replace key
+  # store config into ProgramData folder
+  (Get-Content -Path "$INSTALLFOLDER\logdna.conf.sample") -Replace "<YOUR_INGESTION_KEY>", "$INGESTION_KEY" | Set-Content -Path "$DATAFOLDER\logdna.conf"
+} else {
+  (Get-Content -Path "$DATAFOLDER\logdna.conf") -Replace "<YOUR_INGESTION_KEY>", "$INGESTION_KEY" | Set-Content -Path "$DATAFOLDER\logdna.conf"
+}
 
 Write-Host
-
-Start-Sleep 20
-
 
 
