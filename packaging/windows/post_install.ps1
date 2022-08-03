302d59b2cd6b317ff1b1a9c16df574fb5d8e0a44
Dmitri Khokhlov <dmitri.khokhlov@logdna.com>
2022-08-03 08:30:10 -0700

    fix: missing ProgramData/logdna in clean install

--- packaging/windows/post_install.ps1
+++ packaging/windows/post_install.ps1
@@ -8,15 +8,18 @@ $ErrorActionPreference = 'Stop'
 # set service recovery params
 sc.exe failure MezmoAgentService reset= 60 actions= restart/15000/restart/15000/restart/15000
 
+
 if (-not(Test-Path -Path "$DATAFOLDER\logdna.conf" -PathType Leaf)) {
   # read config template
   # replace key
-  # store config into ProgramData folder
+  # save config to ProgramData folder
+  New-Item -Force -Type Directory -Path "$DATAFOLDER"
   (Get-Content -Path "$INSTALLFOLDER\logdna.conf.sample") -Replace "<YOUR_INGESTION_KEY>", "$INGESTION_KEY" | Set-Content -Path "$DATAFOLDER\logdna.conf"
 } else {
+  # read config
+  # replace key
+  # save config back
   (Get-Content -Path "$DATAFOLDER\logdna.conf") -Replace "<YOUR_INGESTION_KEY>", "$INGESTION_KEY" | Set-Content -Path "$DATAFOLDER\logdna.conf"
 }
 
 Write-Host
-
-
