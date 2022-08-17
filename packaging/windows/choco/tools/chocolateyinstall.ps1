30a7bd10b3837120e5a3b3e7dc632d17990d3656
Dmitri Khokhlov <dmitri.khokhlov@logdna.com>
2022-08-17 16:19:42 -0700

    cleanup

--- packaging/windows/choco/tools/chocolateyinstall.ps1
+++ packaging/windows/choco/tools/chocolateyinstall.ps1
@@ -20,9 +20,4 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
-#https://chocolatey.org/docs/helpers-install-chocolatey-package
 Install-ChocolateyPackage @packageArgs
-## If you are making your own internal packages (organizations), you can embed the installer or
-## put on internal file share and use the following instead (you'll need to add $file to the above)
-# https://chocolatey.org/docs/helpers-install-chocolatey-install-package
-#Install-ChocolateyInstallPackage @packageArgs
