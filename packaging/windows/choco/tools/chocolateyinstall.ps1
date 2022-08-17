ab75b5f8158160687c62ea005e527a275e8091ce
Dmitri Khokhlov <dmitri.khokhlov@logdna.com>
2022-08-17 00:42:52 -0700

    mk_choco, SHA256

--- packaging/windows/choco/tools/chocolateyinstall.ps1
+++ packaging/windows/choco/tools/chocolateyinstall.ps1
@@ -8,15 +8,13 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  url64bit      = 'https://logdna-agent-build-bin.s3.amazonaws.com/3.6.0-beta.1/x86_64-pc-windows-msvc/signed/mezmo-agent.msi'
+  url64bit      = '${MSI_URL}'
   #file         = $fileLocation
 
   softwareName  = 'Mezmo Agent' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = ''
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256'
+  checksum      = '${MSI_SHA256}'
+  checksumType  = 'sha256'
 
   silentArgs    = "/quiet" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
