7957737497f929d74220ac468f8e05a36756ea8a
stsantilena <12675156+stsantilena@users.noreply.github.com>
2022-12-12 12:44:37 -0800

    Configured unix only tests

--- common/fs/src/cache/mod.rs
+++ common/fs/src/cache/mod.rs
@@ -1653,6 +1653,7 @@ mod tests {
 
     // Simulates the `create_copy` log rotation strategy
     #[tokio::test]
+    #[cfg(unix)]
     async fn filesystem_rotate_create_copy() -> io::Result<()> {
         let tempdir = TempDir::new()?;
         let path = tempdir.path().to_path_buf();
@@ -1882,6 +1883,7 @@ mod tests {
 
     // Deletes a file
     #[tokio::test]
+    #[cfg(unix)]
     async fn filesystem_delete_file() -> io::Result<()> {
         let _ = env_logger::Builder::from_default_env().try_init();
         let tempdir = TempDir::new()?;
@@ -2026,6 +2028,7 @@ mod tests {
 
     // Deletes a hardlink
     #[tokio::test]
+    #[cgf(unix)]
     async fn filesystem_delete_hardlink() -> io::Result<()> {
         let tempdir = TempDir::new()?;
         let path = tempdir.path().to_path_buf();
@@ -2051,6 +2054,7 @@ mod tests {
     // Deletes the pointee of a hardlink (not totally accurate since we're not deleting the inode
     // entry, but what evs)
     #[tokio::test]
+    #[cfg(unix)]
     async fn filesystem_delete_hardlink_pointee() -> io::Result<()> {
         let tempdir = TempDir::new()?;
         let path = tempdir.path().to_path_buf();
