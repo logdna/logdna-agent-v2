ea69576b52fba6d3c5e6f7b1cfb9a5082d0c1367
Chris Nixon <chris.nixon@sigma.me.uk>
2023-01-12 16:12:49 +0000

    Tag test_directory_created_after_initialization as it

--- bin/tests/it/cli.rs
+++ bin/tests/it/cli.rs
@@ -1917,6 +1917,7 @@ async fn test_line_redact() {
 
 #[tokio::test]
 #[cfg(unix)]
+#[cfg_attr(not(feature = "integration_tests"), ignore)]
 async fn test_directory_created_after_initialization() {
     let _ = env_logger::Builder::from_default_env().try_init();
     let dir = tempdir().expect("Couldn't create temp dir...").into_path();
