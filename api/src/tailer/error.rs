47ffb07df8692abd92438592b61fd170f2a27661
Dmitri Khokhlov <dkhokhlov@gmail.com>
2023-01-30 11:56:33 -0800

    win32job poc
    # Conflicts:
    #	Cargo.lock
    #	common/fs/src/cache/dir_path.rs

--- api/src/tailer/error.rs
+++ api/src/tailer/error.rs
@@ -2,14 +2,14 @@ use std::fmt::{self, Display, Formatter};
 
 #[derive(Debug)]
 pub enum TailerError {
-    RecordMissingField(String),
+    InvalidJSON(String),
 }
 
 impl Display for TailerError {
     fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
         match self {
-            TailerError::RecordMissingField(field) => {
-                write!(f, "missing journald field {}", field)
+            TailerError::InvalidJSON(error) => {
+                write!(f, "Invalid JSON: {}", error)
             }
         }
     }
